e423dc6f0a12918f80eb0c939cf75fd2d82f66f0
winsoft666 <winsoft666@outlook.com>
2021-07-09 19:01:43 +0800

    Add test project.

--- config.cmake
+++ config.cmake
@@ -11,7 +11,7 @@ SET(LIBVLC_VERSION      0x030014)
 SET(LIBVLC_LIBRARY      "d:/sourcecode/Github/vlc-qt/vlc-3.0.14/sdk/lib/libvlc.lib")
 SET(LIBVLCCORE_LIBRARY  "d:/sourcecode/Github/vlc-qt/vlc-3.0.14/sdk/lib/libvlccore.lib")
 SET(LIBVLC_INCLUDE_DIR  "d:/sourcecode/Github/vlc-qt/vlc-3.0.14/sdk/include")
-
+SET(LIBVLC_BIN_DIR      "d:/sourcecode/Github/vlc-qt/vlc-3.0.14/sdk/bin")
 
 # VLCQt Version
 SET(PROJECT_VERSION_MAJOR 2)
